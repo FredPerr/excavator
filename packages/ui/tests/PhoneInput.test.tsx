d79fb91d13c81b596704e40974a0117f90ddeba5
Frédéric Perron <frederic.perr@gmail.com>
2024-10-09 08:26:40 -0400

    test(ui): change back to src not dist for testing

--- packages/ui/tests/PhoneInput.test.tsx
+++ packages/ui/tests/PhoneInput.test.tsx
@@ -1,7 +1,7 @@
 import { expect, test } from "vitest"
 import { render, screen } from "@testing-library/react"
 import userEvent from "@testing-library/user-event"
-import { PhoneInput } from "../dist/components/PhoneInput"
+import { PhoneInput } from "../src/components/PhoneInput"
 import '@testing-library/jest-dom'
 
 
